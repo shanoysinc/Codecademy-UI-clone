51e0ce1f4ed57f3c93e500c8b825fc2351fc2cda
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-02 22:32:02 -0800

    style navbar

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -3,6 +3,12 @@ import React from "react";
 import logo from "../../assests/svg/codecademy-logo.com.svg";
 import notification from "../../assests/svg/notification.svg";
 import menu from "../../assests/svg/menu.svg";
+import course from "../../assests/svg/agenda.svg";
+import community from "../../assests/svg/chat-group.svg";
+import price from "../../assests/svg/dollar.svg";
+import home from "../../assests/svg/home.svg";
+import book from "../../assests/svg/book.svg";
+import userPhoto from "../../assests/img/images.jpg";
 
 const NavBar = () => {
 	const toggleHandler = () => {
@@ -28,17 +34,59 @@ const NavBar = () => {
 			</div>
 			<div id="navbar__toggle">
 				<div className="navbar__list">
-					<a href="#">My Home</a>
+					<a href="#">
+						<img
+							src={home}
+							alt="home icon"
+							className="navbar__icon--md"
+						/>
+						My Home
+					</a>
 
-					<a href="#"> Course Catalog</a>
+					<a href="#">
+						<img
+							src={course}
+							alt="course icon"
+							className="navbar__icon--md"
+						/>
+						Course Catalog
+					</a>
 
-					<a href="#">Resources</a>
+					<a href="#">
+						<img
+							src={book}
+							alt="book icon"
+							className="navbar__icon--md"
+						/>
+						Resources
+					</a>
 
-					<a href="#">Community</a>
+					<a href="#">
+						<img
+							src={community}
+							alt="community icon"
+							className="navbar__icon--md"
+						/>
+						Community
+					</a>
 
-					<a href="#">Plans + Pricing</a>
+					<a href="#">
+						<img
+							src={price}
+							alt="price icon"
+							className="navbar__icon--md"
+						/>
+						Plans + Pricing
+					</a>
 
-					<a href="#">Shanoy Sinclair</a>
+					<a href="#">
+						<img
+							src={userPhoto}
+							alt="User"
+							className="navbar__icon--md user-photo"
+						/>
+						Shanoy Sinclair
+					</a>
 				</div>
 			</div>
 		</div>
