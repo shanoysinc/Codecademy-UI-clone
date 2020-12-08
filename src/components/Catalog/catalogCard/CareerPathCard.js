938133914a785a31588684308bd30bb7a6c7e38e
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-07 21:37:16 -0800

    remove unused code

--- src/components/Catalog/catalogCard/CareerPathCard.js
+++ src/components/Catalog/catalogCard/CareerPathCard.js
@@ -1,4 +1,3 @@
-import { getByTitle } from "@testing-library/react";
 import React from "react";
 
 const CareerPathCard = ({ title, svg }) => {
--- src/components/footer/CompanyInfo/CompanyInfo.js
+++ src/components/footer/CompanyInfo/CompanyInfo.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import Info from "./Info";
+// import Info from "./Info";
 const CompanyInfo = () => {
 	return (
 		<div className="infoContainer">
