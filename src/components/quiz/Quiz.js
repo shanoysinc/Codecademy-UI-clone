2c11e6681dc10b43dd7eb980751c33ff388db3fe
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-08 21:31:42 -0800

    style quiz box

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,20 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import quizSvg_1 from "../../assests/svg/quiz/quiz-2.svg";
+import quizSvg_2 from "../../assests/svg/quiz/quiz-1.svg";
+
 const Quiz = () => {
 	return (
 		<div className="quiz">
 			<button className="quiz__catalog-btn">Catalog Menu →</button>
 
-			<div className="quiz__box">
+			<a href="#" className="quiz__box">
 				<div className="quiz__box-items">
+					<img src={quizSvg_1} alt="quiz svg" className="quizSvg_1" />
 					<p>Not sure where to begin?</p>
-					<p>
-						<a href="#" className="quiz__box-items-link">
-							Take our quiz →
-						</a>
-					</p>
+					<p className="quiz__box-items-link">Take our quiz →</p>
+					<img src={quizSvg_2} alt="quiz svg" className="quizSvg_2" />
 				</div>
-			</div>
+			</a>
 		</div>
 	);
 };
