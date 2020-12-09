184da223bd7a95e89849ce381b8a89170907788a
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-08 17:57:06 -0800

    create footer course section

--- src/components/footer/CompanyInfo/CompanyInfo.js
+++ src/components/footer/CompanyInfo/CompanyInfo.js
@@ -7,56 +7,56 @@ import AppleStoreIcon from "../../../assests/svg/applestore.svg";
 const CompanyInfo = () => {
 	return (
 		<div className="infoContainer">
-			<div className="info">
-				<h3 className="info__title">Company</h3>
-				<div className="info__items">
+			<div className="footerItems">
+				<h3 className="footerItems__title">Company</h3>
+				<div className="footerItems__items">
 					<a href="#">About</a>
 					<a href="#">We're Hiring</a>
 					<a href="#">Shop</a>
 				</div>
 			</div>
-			<div className="info">
-				<h3 className="info__title">Resources</h3>
-				<div className="info__items">
+			<div className="footerItems">
+				<h3 className="footerItems__title">Resources</h3>
+				<div className="footerItems__items">
 					<a href="#">Blog</a>
 					<a href="#">CheatSheets</a>
 					<a href="#">Articles</a>
 				</div>
 			</div>
 
-			<div className="info">
-				<h3 className="info__title">Support</h3>
-				<div className="info__items">
+			<div className="footerItems">
+				<h3 className="footerItems__title">Support</h3>
+				<div className="footerItems__items">
 					<a href="#">Help Center</a>
 				</div>
 			</div>
-			<div className="info">
-				<h3 className="info__title">Community</h3>
-				<div className="info__items">
+			<div className="footerItems">
+				<h3 className="footerItems__title">Community</h3>
+				<div className="footerItems__items">
 					<a href="#">forums</a>
 					<a href="#">Chapter</a>
 					<a href="#">Events</a>
 				</div>
 			</div>
 
-			<div className="info">
-				<h3 className="info__title">Individual plans</h3>
-				<div className="info__items">
+			<div className="footerItems">
+				<h3 className="footerItems__title">Individual plans</h3>
+				<div className="footerItems__items">
 					<a href="#">Pro membership</a>
 					<a href="#">pro students</a>
 				</div>
 			</div>
-			<div className="info">
-				<h3 className="info__title">Enterprise plans</h3>
-				<div className="info__items">
+			<div className="footerItems">
+				<h3 className="footerItems__title">Enterprise plans</h3>
+				<div className="footerItems__items">
 					<a href="#">for business</a>
 					<a href="#">for education</a>
 				</div>
 			</div>
 
-			<div className="info">
-				<h3 className="info__title">Mobile</h3>
-				<div className="info__items mobileIcon">
+			<div className="footerItems">
+				<h3 className="footerItems__title">Mobile</h3>
+				<div className="footerItems__items mobileIcon">
 					<a href="#">
 						<img
 							src={GooglePlayIcon}
