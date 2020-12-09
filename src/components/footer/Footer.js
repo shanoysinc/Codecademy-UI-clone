872b3b00e18f3afc5d5386c8e857f1eeae7f57dc
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-08 18:19:32 -0800

    create copywrite for footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import CompanyInfo from "./CompanyInfo/CompanyInfo";
 import CourseCatalog from "./CourseCatalog/CourseCatalog";
-
+import CopyWriteInfo from "./copyWriteInfo/CopyWriteInfo";
 const Footer = () => {
 	return (
 		<div className="footerContainer__outer">
 			<div className="footerContainer__inner">
 				<CompanyInfo />
 				<CourseCatalog />
+				<CopyWriteInfo />
 			</div>
 		</div>
 	);
---
+++ src/components/footer/copyWriteInfo/CopyWriteInfo.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+const CopyWriteInfo = () => {
+	return (
+		<div className="copyWriteInfo__Container">
+			<div className="copyWriteInfo">
+				<a href="#">Privacy Policy</a> <span> | </span>
+				<a href="#">Cookie Policy</a> <span> | </span>
+				<a href="#">Do Not Sell My Personal Information</a>
+				<span> | </span>
+				<a href="#">Terms</a>
+			</div>
+			<p className="copyWriteInfo__company">
+				Made with️❤️in NYC © 2020 Codecademy
+			</p>
+		</div>
+	);
+};
+
+//
+export default CopyWriteInfo;
