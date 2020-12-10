32f38fba16839080dccbe795823dbe45e7c7c445
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-09 16:49:21 -0800

    create notification dropdown

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,97 +1,81 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assests/svg/codecademy-logo.com.svg";
 import notification from "../../assests/svg/notification.svg";
 import menu from "../../assests/svg/menu.svg";
-import course from "../../assests/svg/agenda.svg";
-import community from "../../assests/svg/chat-group.svg";
-import price from "../../assests/svg/dollar.svg";
-import home from "../../assests/svg/home.svg";
-import book from "../../assests/svg/book.svg";
-import userPhoto from "../../assests/img/images.jpg";
-
+import DropDownItems from "./toggleComponent/DropDownItems";
+import NotificationItems from "./toggleComponent/NotificationItems";
 const NavBar = () => {
-	const toggleHandler = () => {
+	const [showNotification, setShowNotification] = useState(false);
+	const [showDropdownItems, setShowDropdownItems] = useState(false);
+
+	const notifToggleHandler = () => {
 		const toggle = document.getElementById("navbar__toggle");
-		const navbar = document.querySelector(".navbar");
 		if (toggle.style.display === "block") {
 			toggle.style.display = "none";
-			navbar.style.bottom = null;
+			setShowNotification(() => false);
 		} else {
+			// setShowDropdownItems(() => false);
+			setShowNotification(() => true);
+			toggle.style.display = "block";
+		}
+	};
+	const menuToggleHandler = () => {
+		const toggle = document.getElementById("navbar__toggle");
+		const navbar = document.querySelector(".navbar");
+		const notificationBtn = document.querySelector(
+			".navbar__toggleNofi-btn"
+		);
+		if (toggle.style.display === "block") {
+			toggle.style.display = "none";
+			notificationBtn.style.display = "";
+
+			navbar.style.bottom = null;
+			setShowDropdownItems(() => false);
+		} else {
+			// setShowNotification(() => false);
+			setShowDropdownItems(() => true);
+			console.log(notificationBtn.style.display);
+			notificationBtn.style.display = "none";
 			toggle.style.display = "block";
 			navbar.style.bottom = 0;
 		}
 	};
+
 	return (
 		<div className="navbar">
 			<img src={logo} alt="logo" className="navbar__logo" />
 			<div className="navbar__icons">
-				<img
-					src={notification}
-					alt="notification"
-					className=" navbar__icon--sml"
-				/>
-				<button onClick={toggleHandler} className="navbar__toggle-btn">
+				<button
+					onClick={notifToggleHandler}
+					className="navbar__toggleNofi-btn "
+				>
+					<img
+						src={notification}
+						alt="notification"
+						className=" navbar__icon--sml"
+					/>
+				</button>
+
+				<button
+					onClick={menuToggleHandler}
+					className="navbar__toggleMenu-btn"
+				>
 					<img src={menu} alt="menu" className="navbar__icon--sml" />
 				</button>
 			</div>
 			<div id="navbar__toggle">
-				<div className="navbar__list">
-					<a href="#">
-						<img
-							src={home}
-							alt="home icon"
-							className="navbar__icon--md"
-						/>
-						My Home
-					</a>
-
-					<a href="#">
-						<img
-							src={course}
-							alt="course icon"
-							className="navbar__icon--md"
-						/>
-						Course Catalog
-					</a>
-
-					<a href="#">
-						<img
-							src={book}
-							alt="book icon"
-							className="navbar__icon--md"
-						/>
-						Resources
-					</a>
-
-					<a href="#">
-						<img
-							src={community}
-							alt="community icon"
-							className="navbar__icon--md"
-						/>
-						Community
-					</a>
-
-					<a href="#">
-						<img
-							src={price}
-							alt="price icon"
-							className="navbar__icon--md"
-						/>
-						Plans + Pricing
-					</a>
-
-					<a href="#">
-						<img
-							src={userPhoto}
-							alt="User"
-							className="navbar__icon--md user-photo"
-						/>
-						Shanoy Sinclair
-					</a>
-				</div>
-				<button className="dropdown-btn btn">Try pro for free</button>
+				{showNotification ? <NotificationItems /> : null}
+				{showDropdownItems ? (
+					<>
+						<div className="navbar__list">
+							<DropDownItems />
+						</div>
+						<button className="dropdown-btn btn">
+							Try pro for free
+						</button>
+					</>
+				) : null}
 			</div>
 		</div>
 	);
