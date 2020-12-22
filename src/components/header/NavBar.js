1ebb206279f1d3a10705c2310cecd5a74028b076
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-21 19:49:28 -0800

    fix navbar for desktop display

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -5,26 +5,33 @@ import notification from "../../assests/svg/notification.svg";
 import menu from "../../assests/svg/menu.svg";
 import DropDownItems from "./toggleComponent/DropDownItems";
 import NotificationItems from "./toggleComponent/NotificationItems";
+import NavbarItems from "./navbarItems/NavBarItems";
+import userPhoto from "../../assests/img/images.jpg";
+
 const NavBar = () => {
 	const [showNotification, setShowNotification] = useState(false);
 	const [showDropdownItems, setShowDropdownItems] = useState(false);
 
 	const notifToggleHandler = () => {
 		const toggle = document.getElementById("navbar__toggle");
+		const navbar = document.querySelector(".navbar");
+
 		if (toggle.style.display === "block") {
 			toggle.style.display = "none";
 			setShowNotification(() => false);
+			navbar.style.bottom = null;
 		} else {
 			setShowNotification(() => true);
 			toggle.style.display = "block";
+			navbar.style.bottom = "70%";
 		}
 	};
 	const menuToggleHandler = () => {
 		const toggle = document.getElementById("navbar__toggle");
-		const navbar = document.querySelector(".navbar");
 		const notificationBtn = document.querySelector(
 			".navbar__toggleNofi-btn"
 		);
+		const navbar = document.querySelector(".navbar");
 
 		setShowNotification(() => false);
 
@@ -45,10 +52,14 @@ const NavBar = () => {
 	return (
 		<div className="navbar">
 			<img src={logo} alt="logo" className="navbar__logo" />
+			<div className="navbarItems">
+				<NavbarItems />
+			</div>
+
 			<div className="navbar__icons">
 				<button
 					onClick={notifToggleHandler}
-					className="navbar__toggleNofi-btn "
+					className="navbar__toggleNofi-btn"
 				>
 					<img
 						src={notification}
@@ -64,16 +75,17 @@ const NavBar = () => {
 					<img src={menu} alt="menu" className="navbar__icon--sml" />
 				</button>
 			</div>
+
 			<div id="navbar__toggle">
 				{showNotification ? <NotificationItems /> : null}
 				{showDropdownItems ? (
 					<>
-						<div className="navbar__list">
+						<div className="navbar__mobile-list">
 							<DropDownItems />
+							<button className="dropdown-mobile-btn btn">
+								Try pro for free
+							</button>
 						</div>
-						<button className="dropdown-btn btn">
-							Try pro for free
-						</button>
 					</>
 				) : null}
 			</div>
